67b36d35c7ef295de7e8b52bcf880c83589c8a77
FearlessHyena <shadabambat1@gmail.com>
2019-08-20 11:16:29 +0900

    feature release (#1)
    * adding the contract go file and a basic read
    
    * adding an endpoint for the token owner
    
    * updated the ether connection
    
    * added a check for token before querying further
    
    * added logic to cache transfers to a db
    
    * update run instructions

---
+++ platform/repo/tokenowner.go
@@ -0,0 +1,35 @@
+package repo
+
+import (
+	"github.com/fearlesshyena/djtwebserver/platform"
+	"log"
+)
+
+type GetTokenOwner interface {
+	GetOwner(platform.ContractToken) (TokenOwner, error)
+	platform.TokenExists
+}
+
+type TokenOwner struct {
+	Owner string `json:"owner"`
+}
+
+type Token struct {
+	platform.Repo
+	platform.Cache
+}
+
+func (t *Token) GetOwner(contoken platform.ContractToken) (TokenOwner, error) {
+	if owner, err := t.Repo.GetOwner(contoken); err != nil {
+		log.Fatalln(err)
+		return TokenOwner{}, err
+	} else {
+		return TokenOwner{
+			Owner: owner,
+		}, nil
+	}
+}
+
+func (t *Token) TokenExists(contoken platform.ContractToken) bool {
+	return t.Repo.TokenExists(contoken)
+}
